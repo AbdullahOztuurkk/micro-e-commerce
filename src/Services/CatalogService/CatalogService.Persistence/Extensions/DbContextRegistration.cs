ebb9be5087be7f00b7350817e9599390cfc1387f
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-09-24 15:40:59 +0300

    feat: added extension method related context

---
+++ src/Services/CatalogService/CatalogService.Persistence/Extensions/DbContextRegistration.cs
@@ -0,0 +1,30 @@
+﻿using CatalogService.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatalogService.Persistence.Extensions
+{
+    public static class DbContextRegistration
+    {
+        public static IServiceCollection ConfigureContext(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddEntityFrameworkSqlServer()
+                .AddDbContext<CatalogContext>(opt =>
+                {
+                    opt.UseSqlServer(configuration.GetConnectionString("CatalogContext"),
+                    sqlServerOptionsAction: sqlOptions =>
+                    {
+                        sqlOptions.EnableRetryOnFailure(maxRetryCount: 15, TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
+                    });
+                }
+            );
+            return services;
+        }
+    }
+}
