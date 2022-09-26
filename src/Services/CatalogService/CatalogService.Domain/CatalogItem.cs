81fefea16b7a598c2c2d267eec296f210f1f10dd
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-09-26 14:11:56 +0300

    feat: added base response model

--- src/Services/CatalogService/CatalogService.Domain/CatalogItem.cs
+++ src/Services/CatalogService/CatalogService.Domain/CatalogItem.cs
@@ -7,9 +7,6 @@ namespace CatalogService.Domain
         public string Name { get; set; }
         public string Description { get; set; }
         public Decimal Price { get; set; }
-        public string PictureFileName { get; set; }
-        public string PictureUri { get; set; }
-
 
         //Relationship
         public CatalogBrand CatalogBrand { get; set; }
---
+++ src/Services/CatalogService/CatalogService.Domain/Common/BaseResponse.cs
@@ -0,0 +1,27 @@
+﻿namespace CatalogService.Domain.Common;
+
+public class BaseResponse
+{
+    public bool IsSucceed { get; private set; }
+    public string Message { get; private set; }
+    public int StatusCode { get; private set; }
+
+    public BaseResponse(bool isSucceed, string message, int statusCode)
+    {
+        IsSucceed = isSucceed;
+        Message = message;
+        StatusCode = statusCode;
+    }
+    public BaseResponse(bool isSucceed, int statusCode) : this(isSucceed, String.Empty, statusCode) { }
+}
+
+public class ErrorResponse : BaseResponse
+{
+    public ErrorResponse(string Message) : base(false, Message, 400) { }
+}
+
+public class SuccessResponse : BaseResponse
+{
+    public SuccessResponse(string Message) : base(true, Message, 200) { }
+    public SuccessResponse() : base(true, 200) { }
+}
