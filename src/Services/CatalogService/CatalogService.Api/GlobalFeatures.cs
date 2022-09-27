d728a37746239c3539365efe02238898b018efa4
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-09-27 13:26:52 +0300

    feat: removed unused using blocks

---
+++ src/Services/CatalogService/CatalogService.Api/GlobalFeatures.cs
@@ -0,0 +1,7 @@
+﻿global using CatalogService.Application.Features.Queries.GetCatalogBrandsEvent;
+global using CatalogService.Application.Features.Queries.GetCatalogTypesEvent;
+global using CatalogService.Application.Features.Queries.GetItemsByBrandEvent;
+global using CatalogService.Application.Features.Queries.GetItemsByIdEvent;
+global using CatalogService.Application.Features.Queries.GetItemsByNameEvent;
+global using CatalogService.Application.Features.Queries.GetItemsByTypeAndBrandEvent;
+global using CatalogService.Application.Features.Queries.GetItemsEvent;
