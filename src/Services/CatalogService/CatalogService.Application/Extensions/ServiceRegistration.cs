2e556d063174541c75c907a875c30f42c3323f77
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-09-27 11:09:23 +0300

    feat: extension method has been created

---
+++ src/Services/CatalogService/CatalogService.Application/Extensions/ServiceRegistration.cs
@@ -0,0 +1,17 @@
+﻿using CatalogService.Application.Profiles;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace CatalogService.Application.Extensions
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(CatalogProfile));
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
---
+++ src/Services/CatalogService/CatalogService.Application/Profiles/CatalogProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using CatalogService.Application.Features.Commands.CreateEvent;
+using CatalogService.Application.Features.Commands.UpdateEvent;
+using CatalogService.Domain;
+
+namespace CatalogService.Application.Profiles
+{
+    public  class CatalogProfile : Profile
+    {
+        public CatalogProfile()
+        {
+            CreateMap<UpdateCatalogCommandRequest, CatalogItem>().ReverseMap();
+            CreateMap<CreateCatalogCommandRequest, CatalogItem>().ReverseMap();
+        }
+    }
+}
