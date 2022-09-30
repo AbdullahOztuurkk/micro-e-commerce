c9bdd663684f7f57ce945b2232caa58fdb98ae07
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-09-30 09:30:06 +0300

    feat: all events has been created related notification service

---
+++ src/Services/NotificationService/NotificationService/IntegrationEvents/Handlers/OrderPaymentFailedIntegrationEventHandler.cs
@@ -0,0 +1,23 @@
+﻿using EventBus.MassTransit.RabbitMq.Events.Handlers;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+
+namespace NotificationService.IntegrationEvents.Handlers
+{
+    public class OrderPaymentFailedIntegrationEventHandler : IIntegrationEventHandler<OrderPaymentFailedIntegrationEvent>
+    {
+        private readonly ILogger<OrderPaymentFailedIntegrationEventHandler> logger;
+
+        public OrderPaymentFailedIntegrationEventHandler(ILogger<OrderPaymentFailedIntegrationEventHandler> logger)
+        {
+            this.logger = logger;
+        }
+
+        public Task Consume(ConsumeContext<OrderPaymentFailedIntegrationEvent> context)
+        {
+            //Fake email logic
+            logger.LogInformation($"Order payment has been failed! Order Id: {context.Message.OrderId}");
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/Services/NotificationService/NotificationService/IntegrationEvents/Handlers/OrderPaymentSuccessIntegrationEvent.cs
@@ -0,0 +1,23 @@
+﻿using EventBus.MassTransit.RabbitMq.Events.Handlers;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+
+namespace NotificationService.IntegrationEvents.Handlers
+{
+    public class OrderPaymentSuccessIntegrationEventHandler : IIntegrationEventHandler<OrderPaymentSuccessIntegrationEvent>
+    {
+        private readonly ILogger<OrderPaymentSuccessIntegrationEventHandler> logger;
+
+        public OrderPaymentSuccessIntegrationEventHandler(ILogger<OrderPaymentSuccessIntegrationEventHandler> logger)
+        {
+            this.logger = logger;
+        }
+
+        public Task Consume(ConsumeContext<OrderPaymentSuccessIntegrationEvent> context)
+        {
+            //Fake email logic
+            logger.LogInformation("Order payment has been succeeded!");
+            return Task.CompletedTask;
+        }
+    }
+}
