fbfadf59851c6dbd52138189a54f0b61425a625d
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-09-30 11:28:28 +0300

    feat: extension method has been removed from bus layer

---
+++ src/BuildingBlocks/EventBus/EventBus.MassTransit.RabbitMq/Constants/RabbitMqSettings.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace EventBus.MassTransit.RabbitMq.Configurations
+{
+    public static class RabbitMqConstants
+    {
+        public const string RabbitMQUri = "rabbitmq://localhost/";
+        public const string RabbitMQUserName  = "guest";
+        public const string RabbitMQPassword = "guest";
+    }
+}
---
+++ src/BuildingBlocks/EventBus/EventBus.MassTransit.RabbitMq/ServiceRegistration.cs
@@ -0,0 +1,21 @@
+﻿using EventBus.MassTransit.RabbitMq.Configurations;
+using MassTransit;
+
+namespace EventBus.MassTransit.RabbitMq
+{
+    public static class BusConfigurator
+    {
+        public static IBusControl ConfigureBus(Action<IRabbitMqBusFactoryConfigurator> registrationAction = null)
+        {
+            return Bus.Factory.CreateUsingRabbitMq((configuration) =>
+            {
+                configuration.Host(RabbitMqConstants.RabbitMQUri, hostOpt =>
+                {
+                    hostOpt.Username(RabbitMqConstants.RabbitMQUserName);
+                    hostOpt.Password(RabbitMqConstants.RabbitMQPassword);
+                });
+                registrationAction?.Invoke(configuration);
+            });
+        }
+    }
+}
