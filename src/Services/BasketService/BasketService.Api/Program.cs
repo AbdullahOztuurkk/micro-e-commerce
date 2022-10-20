b84a8d4d675543f7be665a032f892cd45539862a
Abdullah Öztürk <abdullahozturk99@gmail.com>
2022-10-21 00:07:36 +0300

    feat: extension method has been implemented in basket api layer

--- src/Services/BasketService/BasketService.Api/Program.cs
+++ src/Services/BasketService/BasketService.Api/Program.cs
@@ -1,11 +1,39 @@
+using BasketService.Application.Extensions;
+using EventBus.MassTransit.RabbitMq.Constants;
+using MassTransit;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.ConfigureAuth(builder.Configuration);
+builder.Services.AddSingleton(sp => sp.ConfigureRedis(builder.Configuration));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddMassTransit(x =>
+{
+    //x.AddConsumer<BasketEventHandler>();
+    
+    x.SetKebabCaseEndpointNameFormatter();
+
+    x.AddBus(provider => Bus.Factory.CreateUsingRabbitMq(cfg =>
+    {
+        cfg.ConfigureEndpoints(provider);
+
+        cfg.Host(RabbitMqConstants.Uri, h =>
+        {
+            h.Username(RabbitMqConstants.UserName);
+            h.Password(RabbitMqConstants.Password);
+        });
+        cfg.ReceiveEndpoint(RabbitMqConstants.BasketServiceQueueName, ep =>
+        {
+            //ep.ConfigureConsumer<BasketIntegrationEventHandler>(provider);
+        });
+    }));
+});
 
 var app = builder.Build();
 
---
+++ src/Services/BasketService/BasketService.Application/Extensions/RedisRegistration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+
+namespace BasketService.Application.Extensions
+{
+    public static class RedisRegistration
+    {
+        public static ConnectionMultiplexer ConfigureRedis(this IServiceProvider services, IConfiguration configuration)
+        {
+            var redisConfig = ConfigurationOptions.Parse(configuration["RedisSettings:ConnectionString"], true);
+            redisConfig.ResolveDns = true;
+        
+            return ConnectionMultiplexer.Connect(redisConfig);
+        }
+
+    }
+}
